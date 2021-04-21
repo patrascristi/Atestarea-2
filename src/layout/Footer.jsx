15f550bb8660f978cf062ec0284e4c4f8e2d5c09
patrascristi <tbest2070@gmail.com>
2021-04-21 23:30:42 +0300

    apiadd

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return <footer className="page-footer indigo darken-3">
+    <div className="footer-copyright">
+      <div className="container">
+      © {new Date().getFullYear()} Copyright Text
+      </div>
+    </div>
+  </footer>
+}
+
+export {Footer}
---
+++ src/layout/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return <nav className="indigo darken-4">
+        <div className="nav-wrapper">
+        <a href="#" className="brand-logo">Film List</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+        </ul>
+        </div>
+    </nav>
+}
+
+export {Header}
